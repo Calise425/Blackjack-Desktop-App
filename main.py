18e3c6f273e836ad57ef363c0b0cb5feb716f1ad
Calise425 <caitlin.norris1998@gmail.com>
2024-03-29 16:34:09 -0600

    Initial commit, file structure and image assets.

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+deck = {}
+
+
+def deal_card():
+    pass
+
+
+def check_win():
+    pass
+
+
+# -------------------------------- UI SETUP -------------------------------- #
+window = Tk()
+
+canvas = Canvas()
+
+window.mainloop()
