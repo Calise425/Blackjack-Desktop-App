b11222956a84dc472a2011bcbbf29c2cf5f51171
Calise425 <caitlin.norris1998@gmail.com>
2024-03-29 17:39:35 -0600

    Added a deal card function, it's working correctly to deal a card to the respective hand.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from random import randint
+
+window = Tk()
 
 card_2 = PhotoImage(file="assets/2.png")
 card_3 = PhotoImage(file="assets/3.png")
@@ -14,19 +17,50 @@ card_queen = PhotoImage(file="assets/queen.png")
 card_king = PhotoImage(file="assets/king.png")
 card_ace = PhotoImage(file="assets/ace.png")
 
-deck = {}
+
+def deal_card(target_player: list):
+    """This function deals a new random card from the deck to the chosen players hand (dealer or player)"""
+    random_num = randint(2, 14)
+    target_player.append(deck[random_num])
 
 
-def deal_card():
-    pass
+# I assigned a number from 2-14 to these cards to then pick a 'random' card using randint(2,14)
+# The value is saved for the calculating of total hand value, and the card will be used to render in the GUI
+deck = {
+    2: {"value": 2, "image": card_2},
+    3: {"value": 3, "image": card_3},
+    4: {"value": 4, "image": card_4},
+    5: {"value": 5, "image": card_5},
+    6: {"value": 6, "image": card_6},
+    7: {"value": 7, "image": card_7},
+    8: {"value": 8, "image": card_8},
+    9: {"value": 9, "image": card_9},
+    10: {"value": 10, "image": card_10},
+    11: {"value": 10, "image": card_jack},
+    12: {"value": 10, "image": card_queen},
+    13: {"value": 10, "image": card_king},
+    14: {"value": 11, "image": card_ace},
+}
+
+player_hand = []
+dealer_hand = []
+
+print(player_hand, dealer_hand)
+deal_card(player_hand)
+
+print(player_hand)
 
 
 def check_win():
     pass
 
 
+def render_cards():
+    pass
+
+
 # -------------------------------- UI SETUP -------------------------------- #
-window = Tk()
+
 window.title("Blackjack")
 window.config(width=696, height=480)
 
